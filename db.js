c6a6df43a88fdd2e756509b0242020e28e4a87fb
prakashnaxtre <prakash.naxtre@gmail.com>
2024-10-04 10:36:00 +0530

    update

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/video_call_app');
+mongoose.connect('mongodb+srv://prakashnaxtre:sahoo1234@cluster1.mg62mzw.mongodb.net/video_call_app');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
